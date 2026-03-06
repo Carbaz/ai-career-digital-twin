9ef0b6bb75a28f327368804da1fbb8130b4a906a
Carlos Bazaga <cabazaga@gmail.com>
2026-03-06 22:44:43 +0100

    Refine comments in app.py for clarity on demo variable usage

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ summary_txt = "summary.txt"
 repo_id = "Carbaz/career_datastore"
 
 # HBD: We assign the 'app' to 'demo' to allow watch autoreload:
-# 'demo' is required to be defined at the global scope for reload watcher to work
-# in Gradio.
+# 'demo' is required to be defined at the global scope for reload to work in Gradio.
 # Initial warnings may appear at launch with 'gradio app.py' because 'demo' will not
 # exists until the app service ends inside main, but it will do when reload requires it,
 # once the service has been stopped.
